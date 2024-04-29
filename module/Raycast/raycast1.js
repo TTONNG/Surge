63d363baf0efbb1aea98922b9b2fefcb53ff1a64
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-04-30 06:17:10 +0800

    Update raycast1.js

--- module/Raycast/raycast1.js
+++ module/Raycast/raycast1.js
@@ -1,41 +1,51 @@
-let parsedBody = JSON.parse($response.body); 
-const fieldsToSetTrue = [
-    'admin', 'eligible_for_pro_features', 'eligible_for_ai', 'eligible_for_gpt4',
-    'eligible_for_bext', 'eligible_for_file_search_beta', 'eligible_for_ai_beta_features',
-    'can_use_referral_codes', 'eligible_for_cloud_sync', 'eligible_for_application_settings',
-    'can_manage_billing', 'has_running_subscription', 'has_active_subscription',
-    'can_cancel_subscription', 'can_view_billing', 'can_modify_subscription_interval',
-    'any_organization_has_active_subscription', 'any_organization_has_running_subscription',
-    'any_organization_has_better_ai', 'has_pro_features', 'has_better_ai', 'has_developer_extensions',
-    'publishing_bot'
-];
-fieldsToSetTrue.forEach(field => { parsedBody[field] = true; });
+let body = $response.body;
+try {
+  let obj = JSON.parse(body);
+  obj.id = "45ec6a50-8cbe-4e3e-b0a8-e63355a3428b";
+  obj.name = "raycast";
+  obj.handle = "raycast";
+  obj.bio = "";
+  obj.twitter_handle = "";
+  obj.github_handle =   "";
+  obj.location = "USA";
+  obj.initials = "t_";
+  obj.avatar_placeholder_color = "#D64854";
+  obj.avatar = "https://svgl.app/library/raycast.svg";
+  obj.client_flags = {"pro_plan_walkthrough_shown": true};
+  obj.eligible_for_pro_features = true;
+  obj.eligible_for_ai = true;
+  obj.eligible_for_gpt4 = true;
+  obj.eligible_for_developer_hub = true;
+  obj.eligible_for_bext = true;
+  obj.eligible_for_file_search_beta = true;
+  obj.eligible_for_ai_beta_features = true;
+  obj.can_use_referral_codes = true;
+  obj.eligible_for_ai_citations = true;
+  obj.eligible_for_cloud_sync = true;
+  obj.eligible_for_application_settings = true;
+  obj.can_upgrade_to_pro = false;
+  obj.can_manage_billing = true;
+  obj.subscription = {
+    "id": "sub_1OxZKbGdsJ8jfw5XKdbtvQ8S",
+    "status": "active",
+    "interval": "year",
+    "running": true,
+    "has_better_ai": true,
+    "canceled_at": null,
+    "current_period_end": 1913897013,
+    "can_update": true,
+    "can_cancel": true,
+    "can_update_interval": false,
+    "can_upgrade_to_better_ai": false,
+    "can_downgrade_from_better_ai": true
+  };
+  obj.has_pro_features = true;
+  obj.email = "hello@raycast.com";
+  $response.body = JSON.stringify(obj);
+} catch (error) {
+  console.log("Error modifying response body:", error);
+}
 
-Object.assign(parsedBody, {
-    id: "45ec6a50-8cbe-4e3e-b0a8-e63355a3428b",
-    stripe_subscription_status: "active",
-    location: "USA",
-    initials: "TS",
-    username: "raycast",
-    stripe_subscription_id: "sub_1OxZKbGdsJ8jfw5XKdbtvQ8S",
-    name: "raycast"
+$done({
+  body: $response.body
 });
-
-parsedBody.subscription = {
-    id: "sub_1OxZKbGdsJ8jfw5XKdbtvQ8S",
-    can_cancel: true,
-    can_update_interval: false,
-    interval: "year",
-    running: true,
-    current_period_end: 1913897013,
-    can_upgrade_to_better_ai: false,
-    canceled_at: null,
-    can_downgrade_from_better_ai: true,
-    can_update: true,
-    status: "active",
-    has_better_ai: true
-};
-
-let modifyResponseBody = JSON.stringify(parsedBody);
-
-$done({response: { body: modifyResponseBody }});
