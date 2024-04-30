a5e5d93f6dc7ce7e79d7ae96e1fec01bc79952b8
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-04-30 12:45:52 +0800

    Update raycast4.js

--- module/Raycast/raycast4.js
+++ module/Raycast/raycast4.js
@@ -1,7 +1,5 @@
 var headers = $request.headers;
-
+headers['cookie'] = "__raycast_session=d5d306ff08dc68a398a41ac44825510a";
 headers['authorization'] = "Bearer KcSjcbx0Qs0mAK6-9QPBp6hGWl_F4zn_nzLmkU0-UIQ";
-
-
 
 $done({headers});
