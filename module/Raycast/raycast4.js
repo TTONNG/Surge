78284a1f8f78d459dcef33b140b6dd2a75c8480d
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-04-30 12:01:30 +0800

    Update raycast4.js

--- module/Raycast/raycast4.js
+++ module/Raycast/raycast4.js
@@ -1,18 +1,9 @@
-var modifyRequestHeaders = function(request) {
-    // Retrieve existing headers
-    var headers = request.headers;
+var headers = $request.headers;
 
-    // Set new Cookie header
-    headers['cookie'] = '__raycast_session=d5d306ff08dc68a398a41ac44825510a';
+headers['cookie'] = "__raycast_session=d5d306ff08dc68a398a41ac44825510a";
 
-    // Set new Authorization header
-    headers['authorization'] = 'Bearer KcSjcbx0Qs0mAK6-9QPBp6hGWl_F4zn_nzLmkU0-UIQ';
+headers['authorization'] = "Bearer KcSjcbx0Qs0mAK6-9QPBp6hGWl_F4zn_nzLmkU0-UIQ";
 
-    // Log modifications -- useful for debugging
-    console.log('Modified headers:', headers);
 
-    // Return modified request
-    return { headers: headers };
-}
 
-$done(modifyRequestHeaders($request));
+$done({headers});
