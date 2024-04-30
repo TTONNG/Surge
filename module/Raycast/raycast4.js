12137ee19a0e4eb15c68e5954d0a98859695d229
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-04-30 13:15:29 +0800

    Update raycast4.js

--- module/Raycast/raycast4.js
+++ module/Raycast/raycast4.js
@@ -1,5 +1,24 @@
-var headers = $request.headers;
-headers['cookie'] = "__raycast_session=d5d306ff08dc68a398a41ac44825510a";
-headers['authorization'] = "Bearer KcSjcbx0Qs0mAK6-9QPBp6hGWl_F4zn_nzLmkU0-UIQ";
+var modifiedHeaders = $request.headers;
+modifiedHeaders['Authorization'] = 'Bearer KcSjcbx0Qs0mAK6-9QPBp6hGWl_F4zn_nzLmkU0-UIQ';
 
-$done({headers});
+// Ensure the method reflects what you require, this is just an example using POST
+if ($request.method == "POST") {
+    $httpClient.post({
+        url: $request.url,
+        headers: modifiedHeaders,
+        body: $request.body
+    }, function(error, response, data) {
+        if (error) {
+            $done({error: error});
+        } else {
+            $done({response: {
+                status: response.status,
+                headers: response.headers,
+                body: data  // This is the response body from the new request
+            }});
+        }
+    });
+} else {
+    // If not POST, you can send the original request or handle it differently
+    $done({});
+}
