a6e95a1a6f8248eecceda1ef09d126ca69fa6020
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-05-01 03:33:22 +0800

    Update CleanShotX.js

--- module/CleanShotX/CleanShotX.js
+++ module/CleanShotX/CleanShotX.js
@@ -7,6 +7,7 @@ if (data && data.data && data.data.user && data.data.user.storage) {
     data.data.user.team.billing_plan.abilities.can_copy_direct_link = true;
     data.data.user.team.billing_plan.abilities.can_set_expire_after = true;
     data.data.user.team.billing_plan.abilities.can_set_media_password = true;
+    data.data.user.team.remove_cleanshot_branding = true;
     data.data.user.email_verified = true;
     data.data.user.updated_at = "2099-01-11T11:36:16.000000Z";
 } else {
