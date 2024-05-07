15d765521eea99b8122c996b4e3582111bab139f
TTONNG <166768011+TTONNG@users.noreply.github.com>
2024-05-07 15:41:39 +0800

    Delete module/Raycast/raycast2.js

--- module/Raycast/raycast2.js
+++
@@ -1,9 +0,0 @@
-let body = JSON.parse($response.body);
-// Ensure `models` array is present in the body
-if (body.models) {
-    body.models.forEach((model) => {
-        model.in_better_ai_subscription = true;
-        model.requires_better_ai = true;
-    });
-}
-$done({ body: JSON.stringify(body) });
